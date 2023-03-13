c93615e7af7ccaf8be5af43bdc4f6f73680e881c
F-Tomas <fodran.tomas@gmail.com>
2023-03-13 16:09:20 +0100

    Reorganizing and adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="pylfmap",
+    version="0.1",
+    description="Python wrapper for LFmap software",
+    long_description=readme(),
+    author="T. Fodran",
+    author_email="t.fodran@astro.ru.nl",
+    url="https://gitlab.iap.kit.edu/auger-observatory/sandboxes/t.fodran/radiocalibrationtoolkit",
+    packages=find_packages(),
+    install_requires=["healpy", "numpy"],
+)
